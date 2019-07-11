c67eecac8675983a57f10c00c54e8abeb570a3dd
legan- <marasanak@gmail.com>
2019-07-11 22:13:54 +0100

    HEAD: init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>To run tests use the following command in a second terminal window:</p>
+        <code>
+          docker exec -it e1365fe95e07 npm run test
+        </code>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
