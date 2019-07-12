d41789fe2feddd1e42cc7a40b4e51034e35982fa
legan- <marasanak@gmail.com>
2019-07-12 15:11:45 +0100

    master: updated travis file IV

--- src/App.test.js
+++ src/App.test.js
@@ -8,5 +8,11 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 // docker exec -it e1365fe95e07 npm run test
 // to run tests
